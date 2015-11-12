545cbc1e12a9702555d963377c19341b92f85fea
Jinwook Lee <jinwook.interchange@gmail.com>
2015-11-12 01:22:11 -0800

    Added Process Alert

--- reddit/pipelines.py
+++ reddit/pipelines.py
@@ -9,34 +9,16 @@ import pymongo
 
 class mongodbpipeline(object):
 
-	collection_name = 'REDDIT_POSTS'
-
 
 	def __init__(self):
+
 		connection = pymongo.MongoClient('mongodb://localhost:27017/')
 		db = connection['REDDIT_DATABASE']
 		self.collection = db.posts
 		print "MongoDB Pipeline Initiated!"
-		# self.collection.insert(
-		# 	{
-		# 		"test1":	{
-		# 		"key1" : "value1",
-		# 		"key2" : "value2",
-		# 		"key3" : "value3",
-		# 		"key4" : "value4",
-		# 		"key5" : "value5"
-		# 		}
-		# 	}
-		# 	)
-		
-		# self.mongo_uri = 'mongodb://localhost:27017/'
-		# self.mongo_db = 'REDDIT_DATABASE'
-
-	# def close_spider(self, spider):
-	# 	self.connection.close()
 
 	def process_item(self, item, spider):
-		print "Processing items....."
+		print "Reorganizing json files..."
 		post_num = {}
 		postcount = 0
 		value_count = 0
@@ -62,6 +44,7 @@ class mongodbpipeline(object):
 			value_count += 1
 			self.collection.insert(dict(post_num))
 
+		print "Saving jsons to MongoDB"
 
 		
 		
--- reddit/spiders/redditSpider.py
+++ reddit/spiders/redditSpider.py
@@ -3,25 +3,28 @@ from scrapy.selector import Selector
 from reddit.items import redditItems
 
 class redditSpider(scrapy.Spider):
+	#reddit crawler attributes
+
 	name = "reddit"
 	allowed_domains = ["reddit.com"]
 	start_urls = [
 			"https://www.reddit.com/r/gaming/"
 			]
+	print "Crawling..." + name;
 
 	def parse(self, response):
+		print "Extraction and Parsing Initiated!"
 		redditPosts = Selector(response).xpath('//div[@id="siteTable"]')
 		items = []
-		# count = 0
+		
 		for post in redditPosts:
-			# count = count + 1
+			# extracting post titles, links, ratings
 			item = redditItems()
 			item['post_title'] = post.xpath('div/div[2]/p[1]/a/text()').extract()
 			item['post_link'] = post.xpath('div/div[2]/p[1]/a/@href').extract()
 			item['post_rating'] = post.xpath('div/div[1]/div[3]/text()').extract()
 			item['post_comment_link'] = post.xpath('div/div[2]//ul/li[1]/a/@href').extract()
 			items.append(item)
-			print item
 			# item['post_sub'] = post.xpath('div[@class="entry"]/p[@class="tagline"]/a/text()').extract()
 		
 		return items
